45bef54fed7a07366858a8e80aa434f0b6ad6e42
Edson M. Souza <edson.workshop@hotmail.com>
2024-01-12 02:11:49 -0300

    correção no diretorio da env '-v': de .(dot) para /home/edson/Documentos/Projetos/Enviados/apps-cli-docker, passando caminho completo

--- apps-cli-docker.sh
+++ apps-cli-docker.sh
@@ -1,4 +1,4 @@
-echo -e "alias node=\"docker run --rm -v .:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 node\"" >> ~/.bashrc
-echo -e "alias npm=\"docker run --rm -v .:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 npm\"" >> ~/.bashrc
-echo -e "alias nodemon=\"docker run --rm -v .:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 nodemon\"" >> ~/.bashrc
-echo -e "alias browser-sync=\"docker run --rm -v .:/usr/app -w /usr/app -u $(id -u):$(id -g) -p 100:3000 apps-cli-docker:20.9.0 browser-sync\"" >> ~/.bashrc
+echo -e "alias node=\"docker run --rm -v $PWD:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 node\"" >> ~/.bashrc
+echo -e "alias npm=\"docker run --rm -v $PWD:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 npm\"" >> ~/.bashrc
+echo -e "alias nodemon=\"docker run --rm -v $PWD:/usr/app -w /usr/app -u $(id -u):$(id -g) apps-cli-docker:20.9.0 nodemon\"" >> ~/.bashrc
+echo -e "alias browser-sync=\"docker run --rm -v $PWD:/usr/app -w /usr/app -u $(id -u):$(id -g) -p 100:3000 apps-cli-docker:20.9.0 browser-sync\"" >> ~/.bashrc
